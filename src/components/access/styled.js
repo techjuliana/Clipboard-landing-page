ffa476b50f48d8abdd58c7231d2893a0dbc8dcc8
Juliana Bitencourt <julianasouzabittencourt@gmail.com>
2022-10-28 10:25:30 -0300

    ajustes ✅

--- src/components/access/styled.js
+++ src/components/access/styled.js
@@ -5,7 +5,6 @@ export const Container = styled.body`
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  flex-wrap: wrap;
   p {
     margin: 2rem;
     text-align: center;
